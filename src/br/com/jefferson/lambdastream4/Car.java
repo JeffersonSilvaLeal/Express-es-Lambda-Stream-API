e2c1089de0e6d2d3ec3e1d3a04d61faca612d74d
JeffersonSIlva <drankhar@gmail.com>
2022-04-08 16:08:43 -0300

    Exemplo de Stream API em coleções de objetos

---
+++ src/br/com/jefferson/lambdastream4/Car.java
@@ -0,0 +1,34 @@
+package br.com.jefferson.lambdastream4;
+
+public class Car {
+
+	private String brand;
+	private double price;
+
+	public Car(String brand, double price) {
+		this.brand = brand;
+		this.price = price;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+	
+	public void addTax() {
+		price = price += price * 0.1;
+	}
+
+	@Override
+	public String toString() {
+		return "Carro [brand=" + brand + ", price=" + price + "]";
+	}
+	
+	
+	
+	
+	
+}
